95b53764d70cc09318fa4a87fcf3ffe27f78d290
dani142c <80045438+dani142c@users.noreply.github.com>
2021-03-04 13:45:55 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+
+dependency "vrp"
+
+client_scripts {
+    "config.lua",
+    "client.lua"
+  }
+  server_scripts {
+    "@vrp/lib/utils.lua",
+    "config.lua",
+    "server.lua"
+  }
---
+++ client.lua
@@ -0,0 +1,8 @@
+RegisterCommand("onduty", function()
+    TriggerServerEvent("gh:onDuty")
+end)
+
+RegisterCommand("offduty", function()
+    TriggerServerEvent("gh:offDuty")
+end)
+
